e090366f0ffb516037c1fa87213e01c8f5878519
Divy Mav <divymav5@gmail.com>
2025-02-02 18:49:31 +0530

    correction

--- event_management/lib/database/models/user.model.ts
+++ event_management/lib/database/models/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, unique: true },
+  clerkId: { type: String, required: true, unique: true },
   email: { type: String, unique: true },
   username: { type: String, unique: true },
   firstName: { type: String },
